8dee2ce938942cd532107743b31cd3d502cdfa48
Viktor <vasilevskiviktor@yahoo.com>
2026-03-06 09:40:51 +0100

    extract premium strategy DI registration into separate extension class

---
+++ Claims.Application/Extensions/PremiumStrategyCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Claims.Application.Interfaces;
+using Claims.Application.Services;
+using Claims.Application.Strategies;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Claims.Application.Extensions;
+
+public static class PremiumStrategyCollectionExtensions
+{
+    public static IServiceCollection AddPremiumStrategies(this IServiceCollection services)
+    {
+        services.AddSingleton<IPremiumRateStrategy, YachtRateStrategy>();
+        services.AddSingleton<IPremiumRateStrategy, TankerRateStrategy>();
+        services.AddSingleton<IPremiumRateStrategy, PassengerShipRateStrategy>();
+        services.AddSingleton<IPremiumRateStrategy, ContainerShipRateStrategy>();
+        services.AddSingleton<IPremiumRateStrategy, BulkCarrierRateStrategy>();
+        services.AddSingleton<IPremiumCalculator, PremiumCalculator>();
+
+        return services;
+    }
+}
--- Claims.Application/Extensions/ServiceCollectionExtensions.cs
+++ Claims.Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using Claims.Application.Channels;
 using Claims.Application.Interfaces;
 using Claims.Application.Services;
-using Claims.Application.Strategies;
 using Claims.Application.Validations.Claims;
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,18 +14,12 @@ public static class ServiceCollectionExtensions
         services.AddScoped<IClaimsService, ClaimsService>();
         services.AddScoped<ICoversService, CoversService>();
         services.AddScoped<IAuditService, AuditService>();
-        services.AddScoped<IPremiumCalculator, PremiumCalculator>();
 
         services.AddValidatorsFromAssemblyContaining<CreateClaimRequestValidator>(ServiceLifetime.Transient);
 
         services.AddSingleton<AuditChannel>();
 
-        services.AddSingleton<IPremiumRateStrategy, YachtRateStrategy>();
-        services.AddSingleton<IPremiumRateStrategy, TankerRateStrategy>();
-        services.AddSingleton<IPremiumRateStrategy, PassengerShipRateStrategy>();
-        services.AddSingleton<IPremiumRateStrategy, ContainerShipRateStrategy>();
-        services.AddSingleton<IPremiumRateStrategy, BulkCarrierRateStrategy>();
-        services.AddSingleton<IPremiumCalculator, PremiumCalculator>();
+        services.AddPremiumStrategies();
 
         return services;
     }
