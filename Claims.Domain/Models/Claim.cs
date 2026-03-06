0158658467c4b9c53321a6ca847379cab5a91d7b
Viktor <vasilevskiviktor@yahoo.com>
2026-03-06 11:13:27 +0100

    add required props for Claim model

--- Claims.Domain/Models/Claim.cs
+++ Claims.Domain/Models/Claim.cs
@@ -9,13 +9,13 @@ public class Claim
     public string? Id { get; set; }
 
     [BsonElement("coverId")]
-    public string CoverId { get; set; }
+    public required string CoverId { get; set; }
 
     [BsonElement("created")]
     public DateTime Created { get; set; }
 
     [BsonElement("name")]
-    public string Name { get; set; }
+    public required string Name { get; set; }
 
     [BsonElement("claimType")]
     public ClaimType Type { get; set; }
