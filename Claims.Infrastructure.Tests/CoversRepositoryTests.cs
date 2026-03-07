3324518c6088ca5b3cc85f1c7bb6dbf1397d3962
Viktor <vasilevskiviktor@yahoo.com>
2026-03-07 19:17:26 +0100

    add unit test for claims and cover repo delete method

---
+++ Claims.Infrastructure.Tests/CoversRepositoryTests.cs
@@ -0,0 +1,42 @@
+﻿using Claims.Domain.Exceptions;
+using Claims.Domain.Models;
+using Claims.Infrastructure.Context;
+using Claims.Infrastructure.Repositories;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Claims.Infrastructure.Tests;
+
+public class CoversRepositoryTests
+{
+    private ClaimsContext CreateInMemoryContext()
+    {
+        var options = new DbContextOptionsBuilder<ClaimsContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+        return new TestClaimsContext(options);
+    }
+
+    private class TestClaimsContext(DbContextOptions options) : ClaimsContext(options)
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Claim>().Property(x => x.Type).HasConversion<string>();
+            modelBuilder.Entity<Cover>().Property(x => x.Type).HasConversion<string>();
+        }
+    }
+
+    [Fact]
+    public async Task DeleteCoverAsync_WhenCoverDoesNotExist_ShouldThrowCoverNotFoundException()
+    {
+        // Arrange
+        await using var context = CreateInMemoryContext();
+        var sut = new CoversRepository(context);
+
+        // Act
+        var act = async () => await sut.DeleteCoverAsync("non-existent-id");
+
+        // Assert
+        await act.Should().ThrowAsync<CoverNotFoundException>();
+    }
+}
