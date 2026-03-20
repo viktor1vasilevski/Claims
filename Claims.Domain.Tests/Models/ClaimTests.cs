d30b67ac4d3378874baf3ea7de0b0b09af21f545
Viktor <vasilevskiviktor@yahoo.com>
2026-03-20 16:45:20 +0100

    test: add Claims.Domain.Tests project with Claim and Cover domain model tests

---
+++ Claims.Domain.Tests/Models/ClaimTests.cs
@@ -0,0 +1,67 @@
+using Claims.Domain.Enums;
+using Claims.Domain.Exceptions;
+using Claims.Domain.Models;
+using FluentAssertions;
+
+namespace Claims.Domain.Tests.Models;
+
+public class ClaimTests
+{
+    private static readonly Guid ValidCoverId = Guid.NewGuid();
+
+    [Fact]
+    public void Create_WhenValid_ShouldReturnClaimWithCorrectProperties()
+    {
+        var created = new DateTime(2026, 6, 1);
+
+        var claim = Claim.Create(ValidCoverId, "Hull damage", ClaimType.Collision, 5000m, created);
+
+        claim.Id.Should().NotBe(Guid.Empty);
+        claim.CoverId.Should().Be(ValidCoverId);
+        claim.Name.Should().Be("Hull damage");
+        claim.Type.Should().Be(ClaimType.Collision);
+        claim.DamageCost.Should().Be(5000m);
+        claim.Created.Should().Be(created);
+    }
+
+    [Fact]
+    public void Create_ShouldGenerateUniqueIds()
+    {
+        var claim1 = Claim.Create(ValidCoverId, "Claim 1", ClaimType.Fire, 1000m, DateTime.UtcNow);
+        var claim2 = Claim.Create(ValidCoverId, "Claim 2", ClaimType.Fire, 1000m, DateTime.UtcNow);
+
+        claim1.Id.Should().NotBe(claim2.Id);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(-100)]
+    public void Create_WhenDamageCostIsZeroOrNegative_ShouldThrowInvalidDamageCostException(decimal damageCost)
+    {
+        var act = () => Claim.Create(ValidCoverId, "Test", ClaimType.Collision, damageCost, DateTime.UtcNow);
+
+        act.Should().Throw<InvalidDamageCostException>();
+    }
+
+    [Theory]
+    [InlineData(100_001)]
+    [InlineData(200_000)]
+    public void Create_WhenDamageCostExceedsLimit_ShouldThrowInvalidDamageCostException(decimal damageCost)
+    {
+        var act = () => Claim.Create(ValidCoverId, "Test", ClaimType.Collision, damageCost, DateTime.UtcNow);
+
+        act.Should().Throw<InvalidDamageCostException>();
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(50_000)]
+    [InlineData(100_000)]
+    public void Create_WhenDamageCostIsWithinBounds_ShouldNotThrow(decimal damageCost)
+    {
+        var act = () => Claim.Create(ValidCoverId, "Test", ClaimType.Collision, damageCost, DateTime.UtcNow);
+
+        act.Should().NotThrow();
+    }
+}
---
+++ Claims.Domain.Tests/Models/CoverTests.cs
@@ -0,0 +1,105 @@
+using Claims.Domain.Enums;
+using Claims.Domain.Exceptions;
+using Claims.Domain.Models;
+using FluentAssertions;
+
+namespace Claims.Domain.Tests.Models;
+
+public class CoverTests
+{
+    private static readonly DateTime Start = new(2026, 1, 1);
+    private static readonly DateTime End = new(2026, 12, 31);
+
+    [Fact]
+    public void Create_WhenValid_ShouldReturnCoverWithCorrectProperties()
+    {
+        var cover = Cover.Create(Start, End, CoverType.Yacht, 50000m);
+
+        cover.Id.Should().NotBe(Guid.Empty);
+        cover.StartDate.Should().Be(Start);
+        cover.EndDate.Should().Be(End);
+        cover.Type.Should().Be(CoverType.Yacht);
+        cover.Premium.Should().Be(50000m);
+    }
+
+    [Fact]
+    public void Create_ShouldGenerateUniqueIds()
+    {
+        var cover1 = Cover.Create(Start, End, CoverType.Yacht, 0m);
+        var cover2 = Cover.Create(Start, End, CoverType.Yacht, 0m);
+
+        cover1.Id.Should().NotBe(cover2.Id);
+    }
+
+    [Fact]
+    public void Create_WhenEndDateEqualsStartDate_ShouldThrowInvalidCoverPeriodException()
+    {
+        var act = () => Cover.Create(Start, Start, CoverType.Yacht, 0m);
+
+        act.Should().Throw<InvalidCoverPeriodException>();
+    }
+
+    [Fact]
+    public void Create_WhenEndDateBeforeStartDate_ShouldThrowInvalidCoverPeriodException()
+    {
+        var act = () => Cover.Create(End, Start, CoverType.Yacht, 0m);
+
+        act.Should().Throw<InvalidCoverPeriodException>();
+    }
+
+    [Fact]
+    public void Create_WhenPeriodExceedsOneYear_ShouldThrowInvalidCoverPeriodException()
+    {
+        var act = () => Cover.Create(Start, Start.AddDays(366), CoverType.Yacht, 0m);
+
+        act.Should().Throw<InvalidCoverPeriodException>();
+    }
+
+    [Fact]
+    public void Create_WhenPeriodIsExactlyOneYear_ShouldNotThrow()
+    {
+        var act = () => Cover.Create(Start, Start.AddDays(365), CoverType.Yacht, 0m);
+
+        act.Should().NotThrow();
+    }
+
+    [Fact]
+    public void IsDateWithinPeriod_WhenDateIsWithinPeriod_ShouldReturnTrue()
+    {
+        var cover = Cover.Create(Start, End, CoverType.Yacht, 0m);
+
+        cover.IsDateWithinPeriod(new DateTime(2026, 6, 15)).Should().BeTrue();
+    }
+
+    [Fact]
+    public void IsDateWithinPeriod_WhenDateIsOnStartBoundary_ShouldReturnTrue()
+    {
+        var cover = Cover.Create(Start, End, CoverType.Yacht, 0m);
+
+        cover.IsDateWithinPeriod(Start).Should().BeTrue();
+    }
+
+    [Fact]
+    public void IsDateWithinPeriod_WhenDateIsOnEndBoundary_ShouldReturnTrue()
+    {
+        var cover = Cover.Create(Start, End, CoverType.Yacht, 0m);
+
+        cover.IsDateWithinPeriod(End).Should().BeTrue();
+    }
+
+    [Fact]
+    public void IsDateWithinPeriod_WhenDateIsBeforeStartDate_ShouldReturnFalse()
+    {
+        var cover = Cover.Create(Start, End, CoverType.Yacht, 0m);
+
+        cover.IsDateWithinPeriod(Start.AddDays(-1)).Should().BeFalse();
+    }
+
+    [Fact]
+    public void IsDateWithinPeriod_WhenDateIsAfterEndDate_ShouldReturnFalse()
+    {
+        var cover = Cover.Create(Start, End, CoverType.Yacht, 0m);
+
+        cover.IsDateWithinPeriod(End.AddDays(1)).Should().BeFalse();
+    }
+}
