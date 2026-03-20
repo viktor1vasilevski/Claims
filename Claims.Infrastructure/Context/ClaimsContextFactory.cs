4d136e7d680bd480c7068774f473fcf3447ab0a2
Viktor <vasilevskiviktor@yahoo.com>
2026-03-20 23:56:02 +0100

    fix: add design-time factory for EF migrations

---
+++ Claims.Infrastructure/Context/ClaimsContextFactory.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Claims.Infrastructure.Context;
+
+public class ClaimsContextFactory : IDesignTimeDbContextFactory<ClaimsContext>
+{
+    public ClaimsContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddEnvironmentVariables()
+            .Build();
+
+        var connectionString = configuration.GetConnectionString("SqlServer")
+            ?? throw new InvalidOperationException("ConnectionStrings:SqlServer environment variable is not set.");
+
+        var options = new DbContextOptionsBuilder<ClaimsContext>()
+            .UseSqlServer(connectionString)
+            .Options;
+
+        return new ClaimsContext(options);
+    }
+}
