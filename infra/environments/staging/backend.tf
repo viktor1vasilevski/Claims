650fd43e40a64d37691ba1bf83f84a9e5fb854cd
Viktor <vasilevskiviktor@yahoo.com>
2026-03-20 20:14:04 +0100

    modify storage_account_name

--- infra/environments/staging/backend.tf
+++ infra/environments/staging/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name = "rg-claims-tfstate"
     # Replace with the storage account name printed by infra/bootstrap/create-backend.sh
-    storage_account_name = "stclaimstfstate"
+    storage_account_name = "stclaimstfstate3989"
     container_name       = "tfstate"
     key                  = "staging.terraform.tfstate"
     # Storage key is supplied via ARM_ACCESS_KEY env var in CI (TF_STATE_STORAGE_KEY secret)
